2109487c3b9dd72be3039d777ce7a1d45f5464e6
Simone Ebert <sebert@hn2.medizin.uni-tuebingen.de>
2024-12-20 16:59:09 +0100

    grid step

---
+++ Brian/lib/.ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,26 @@
+
+# set parameter 
+
+
+
+
+# define the model 
+
+
+# simulate to given stimulus
+
+
+# save output to file 
+
+
+
+
+
+# measure anticipation
+# measure amplitude 
+
+
+
+# plot 
+
+# loop over speed/param and only save certain things
